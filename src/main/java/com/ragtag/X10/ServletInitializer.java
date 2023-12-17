6bca4d96ace4a6389619f0813138592733c02ef3
XinXe <jaesin463@gmail.com>
2023-12-17 20:19:01 +0900

    pom.xml

--- src/main/java/com/ragtag/X10/ServletInitializer.java
+++ src/main/java/com/ragtag/X10/ServletInitializer.java
@@ -3,7 +3,7 @@ package com.ragtag.X10;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+	public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
