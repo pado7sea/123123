ed61d0113756a5280c19acd3271e8931a7ddf965
pado7sea <pado7sea@gmail.com>
2023-12-20 09:27:00 +0900

    Feat: 객관식 CRUD 초안

---
+++ src/main/java/com/ragtag/X10/model/service/MultipleChoiceService.java
@@ -0,0 +1,4 @@
+package com.ragtag.X10.model.service;
+
+public interface MultipleChoiceService {
+}
---
+++ src/main/java/com/ragtag/X10/model/service/MultipleChoiceServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ragtag.X10.model.service;
+
+public class MultipleChoiceServiceImpl {
+}
