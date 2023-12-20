478b9e48fd26b7e93141f85bff0e4b6b9be8e24b
XinXe <jaesin463@gmail.com>
2023-12-20 17:32:13 +0900

    Fix: 로그인 방식 해싱에서 일반 방식으로 변경

--- src/main/java/com/ragtag/X10/controller/UserController.java
+++ src/main/java/com/ragtag/X10/controller/UserController.java
@@ -30,37 +30,50 @@ public class UserController {
         return new ResponseEntity<Integer>(result, HttpStatus.CREATED);
     }
 
-    @GetMapping("/login")
-    public ResponseEntity<Map<String, Object>> selectAll(@RequestBody User user) {
-        Map<String, Object> result = new HashMap<>();
-
-        // 클라이언트에서 보내온 비밀번호를 해싱
-        String hashedPassword = user.getUserPassword();
-        user.setUserPassword(hashedPassword);
-
-        // 서비스에서 사용자 검증을 수행
-        User loginUserInfo = userService.loginUser(user);
-
-        System.out.println(hashedPassword);
-
-        if (loginUserInfo != null) {
-            // 로그인 성공: JWT 토큰 생성 및 반환
-            try {
-                String token = jwtUtil.createToken("id", loginUserInfo.getUserId());
-                result.put("user", loginUserInfo);
-                result.put("access-token", token);
-                result.put("message", "success");
-                return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
-            } catch (UnsupportedEncodingException e) {
-                result.put("message", "fail");
-                return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        } else {
-            // 로그인 실패
-            result.put("message", "fail");
-            return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @PostMapping("/login")
+    public ResponseEntity<?> selectAll(@RequestBody User user) {
+        User result = userService.userInfo(user.getUserId());
+        // 아이디 없음
+        if (result == null)
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        //  비밀번호 불일치
+        if (!result.getUserPassword().equals(user.getUserPassword()))
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        // 로그인 성공
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+//    @PostMapping("/login")
+//    public ResponseEntity<Map<String, Object>> selectAll(@RequestBody User user) {
+//        Map<String, Object> result = new HashMap<>();
+//
+//        // 클라이언트에서 보내온 비밀번호를 해싱
+//        String hashedPassword = user.getUserPassword();
+//        user.setUserPassword(hashedPassword);
+//
+//        // 서비스에서 사용자 검증을 수행
+//        User loginUserInfo = userService.loginUser(user);
+//
+//        System.out.println(hashedPassword);
+//
+//        if (loginUserInfo != null) {
+//            // 로그인 성공: JWT 토큰 생성 및 반환
+//            try {
+//                String token = jwtUtil.createToken("id", loginUserInfo.getUserId());
+//                result.put("user", loginUserInfo);
+//                result.put("access-token", token);
+//                result.put("message", "success");
+//                return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+//            } catch (UnsupportedEncodingException e) {
+//                result.put("message", "fail");
+//                return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+//            }
+//        } else {
+//            // 로그인 실패
+//            result.put("message", "fail");
+//            return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 
     @GetMapping("/read/{userId}")
     public ResponseEntity<?> getUserInfo(@PathVariable("userId") String userId) {
@@ -91,7 +104,7 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<?> deleteUser(@PathVariable("userId") String userId){
+    public ResponseEntity<?> deleteUser(@PathVariable("userId") String userId) {
         int result = userService.deleteUser(userId);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
